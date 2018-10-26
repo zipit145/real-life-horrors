25cdaaa5fa35ca7cdec7c0686640200be0b29ae9
zipit145 <gregbforney@gmail.com>
2018-10-26 10:48:03 -0600

    earthquake api

--- src/App.js
+++ src/App.js
@@ -10,6 +10,21 @@ import Main from './components/Main';
 import Trump from './components/Trump';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      earthquake: []
+    }
+  }
+  async componentDidMount() {
+    const response = await fetch('https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02')
+    const json = await response.json()
+    this.setState({
+      earthquake: json
+    })
+    console.log(this.state.earthquake)
+  }
+  
   render() {
     return (
       <div className="App">
